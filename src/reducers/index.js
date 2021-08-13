bff3c111496c92a4dd8ae2d23fcf94eeb73fe561
ben <ejrrlabc@gmail.com>
2021-08-13 21:58:03 +0900

    clean cached

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import mainReducer from "./mainReducer";
-import loginReducer from "./loginReducer";
-
-const rootReducer = combineReducers({ loginReducer, mainReducer });
-
-export default rootReducer;
